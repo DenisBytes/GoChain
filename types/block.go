b49068d885d0a3439d64b3062af5ac28d20d3b9e
DenisBytes <denisbytes@gmail.com>
2024-02-08 22:34:58 +0000

    [nuild]: added block and grpc

---
+++ types/block.go
@@ -0,0 +1,24 @@
+package types
+
+import (
+	"crypto/sha256"
+
+	"github.com/DenisBytes/GoChain/crypto"
+	"github.com/DenisBytes/GoChain/proto"
+	pb "google.golang.org/protobuf/proto"
+)
+
+// Hashblock returns a SHA-256 of the header.
+func HashBlock(block *proto.Block) []byte {
+	b, err := pb.Marshal(block)
+	if err != nil {
+		panic(err)
+	}
+	hash := sha256.Sum256(b)
+
+	return hash[:]
+}
+
+func SignBlock(pk *crypto.PrivateKey, b *proto.Block) *crypto.Signature {
+	return pk.Sign(HashBlock(b))
+}
---
+++ types/block_test.go
@@ -0,0 +1,28 @@
+package types
+
+import (
+	"testing"
+
+	"github.com/DenisBytes/GoChain/crypto"
+	"github.com/DenisBytes/GoChain/util"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHashBlock(t *testing.T) {
+	block := util.RandomBlock()
+	hash := HashBlock(block)
+	assert.Equal(t, 32, len(hash))
+}
+
+func TestSignBlock(t *testing.T) {
+	var (
+		block   = util.RandomBlock()
+		privKey = crypto.GeneratePrivateKey()
+		pubKey  = privKey.Public()
+	)
+
+	sig := SignBlock(privKey, block)
+	assert.Equal(t, 64, len(sig.Bytes()))
+	assert.True(t, sig.Verify(pubKey, HashBlock(block)))
+
+}
